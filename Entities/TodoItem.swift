d6a7dbd136664fd94a2e74cdfd6135f2a8d10398
villrait <166745091+villrait@users.noreply.github.com>
2025-09-25 16:02:49 +0500

    Add basic project structure with services folders and TodoItem model

---
+++ Entities/TodoItem.swift
@@ -0,0 +1,16 @@
+//
+//  TodoItem.swift
+//  EffectiveMobileToDoList
+//
+//  Created by м on 25.09.2025.
+//
+
+import Foundation
+
+struct TodoItem {
+    let id: Int
+    let title: String
+    let description: String?
+    let isCompleted: Bool
+    let createdAt: Date
+}
---
+++ Services/Network/NetworkServiceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkServiceProtocol.swift
+//  EffectiveMobileToDoList
+//
+//  Created by м on 25.09.2025.
+//
+
+import Foundation
