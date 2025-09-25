223cd28a918d4c34e66c40771389bd6c29bbdd44
villrait <166745091+villrait@users.noreply.github.com>
2025-09-25 17:41:25 +0500

    Connect UITableViewDataSource with test data

--- Moduls/TaskList/TaskListViewController.swift
+++ Moduls/TaskList/TaskListViewController.swift
@@ -14,18 +14,30 @@ protocol TaskListViewControllerProtocol: AnyObject {
 class TaskListViewController: UIViewController, TaskListViewControllerProtocol {
     var presenter: TaskListPresenterProtocol?
     
-    var identifier = "Cell"
-    
     private let tableView = UITableView()
+    private let cellIdentifier = "Cell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        tableView.dataSource = self
     }
     
     private func setupTableView() {
         view.addSubview(tableView)
         tableView.frame = view.bounds
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+}
+
+extension TaskListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "Задача \(indexPath.row + 1)"
+        return cell
     }
 }
