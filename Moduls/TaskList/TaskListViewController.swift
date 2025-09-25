3127474ae10a870f436b34c4bc6d0ba7213b597e
villrait <166745091+villrait@users.noreply.github.com>
2025-09-25 17:35:30 +0500

    Add UITableView to TaskListViewController

--- Moduls/TaskList/TaskListViewController.swift
+++ Moduls/TaskList/TaskListViewController.swift
@@ -13,4 +13,19 @@ protocol TaskListViewControllerProtocol: AnyObject {
 
 class TaskListViewController: UIViewController, TaskListViewControllerProtocol {
     var presenter: TaskListPresenterProtocol?
+    
+    var identifier = "Cell"
+    
+    private let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+    }
 }
