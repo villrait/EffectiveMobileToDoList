78deb0fac78bbf0c73f1659b0e059409787d535a
villrait <166745091+villrait@users.noreply.github.com>
2025-09-25 15:34:34 +0500

    Add initial Xcode project with Core Data support

---
+++ EffectiveMobileToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EffectiveMobileToDoList
+//
+//  Created by м on 25.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
