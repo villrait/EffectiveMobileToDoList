4f683ec28f9dc955c502601d60dc3224d593e62b
villrait <166745091+villrait@users.noreply.github.com>
2025-10-01 01:36:55 +0500

    chore: remove unused ViewController from initial template

--- EffectiveMobileToDoList/SceneDelegate.swift
+++ EffectiveMobileToDoList/SceneDelegate.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    // MARK: - Properties
+    
     var window: UIWindow?
 
+    // MARK: - UISceneDelegate
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
@@ -21,13 +24,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let taskListVC = diContainer.makeTaskListModule()
         window?.rootViewController = UINavigationController(rootViewController: taskListVC)
         window?.makeKeyAndVisible()
-        
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
-
-
 }
-
